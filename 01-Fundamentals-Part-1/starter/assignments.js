f21c8beb8c0ca3cd28cf3f6163c18abc728914da
balli1cm <carterballinger21@hotmail.com>
2022-06-28 21:16:01 -0400

    Added my project

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+let country = "United States";
+let continent = "North America";
+let population = 329500000;
+
+console.log(
+  "Country:",
+  country,
+  "\nContinent:",
+  continent,
+  "\nPopulation:",
+  population
+);
+
+console.log(continent);
+console.log(population);
